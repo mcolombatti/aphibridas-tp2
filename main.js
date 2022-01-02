1aa59727e42cc76722d67413965ce151afb184b8
mcolombatti <maxicolombatti@gmail.com>
2022-01-01 21:32:57 -0300

    deploy heroku

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import empleadosApiRouter from './router/empleadosApi.router.js'
 import beneficiosApiRouter from './router/beneficiosApi.router.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path';
+const __dirname = path.resolve();
 dotenv.config()
 
 const app = express()
@@ -18,6 +20,16 @@ app.use('/user', userAuthRouter)
 app.use("/api/empleados", empleadosApiRouter);
 app.use("/api/beneficios", beneficiosApiRouter);
 //app.use("/home", pageRouter); 
+// ... other imports 
 
-app.listen(9001, function () {
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+const port = process.env.PORT || 9001
+app.listen(port, function () {
 })
